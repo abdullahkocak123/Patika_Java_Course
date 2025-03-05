85d4d9c40adcef55938934b52eaab762846b8fad
Abdullah <abdullahkocak33@hotmail.com>
2025-03-05 10:03:05 +0300

    GroceryCashier example added

---
+++ src/main/java/GroceryCashier.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class GroceryCashier {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Armut kaç kilo?: ");
+        double armut = scanner.nextDouble();
+
+        System.out.print("Elma kaç kilo? ");
+        double elma = scanner.nextDouble();
+
+        System.out.print("Domates kaç kilo? ");
+        double domates = scanner.nextDouble();
+
+        System.out.print("Muz kaç kilo? ");
+        double muz = scanner.nextDouble();
+
+        System.out.print("Patlıcan kaç kilo? ");
+        double patlican = scanner.nextDouble();
+
+
+
+        System.out.print("Toplam Tutar: " + (armut* 2.14  + elma*3.67 + domates*1.11 + muz*0.95 + patlican*5) + " TL");
+
+    }
+}
