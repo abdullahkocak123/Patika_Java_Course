80869cdcc173125bf5303a592f20c2c6063fb116
Abdullah <abdullahkocak33@hotmail.com>
2025-03-06 11:56:28 +0300

    ArithmeticOperationsAndOperationPriority example added

---
+++ src/main/java/ArithmeticOperationsAndOperationPriority.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ArithmeticOperationsAndOperationPriority {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("İlk sayıyı giriniz: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("İkinci sayıyı giriniz: ");
+        int secondNumber = scanner.nextInt();
+
+        System.out.print("Üçüncü sayıyı giriniz: ");
+        int thirdNumber = scanner.nextInt();
+
+        int result = firstNumber + secondNumber * thirdNumber - secondNumber;
+
+        System.out.println("a+b*c-b işlem sonucu: " + result);
+    }
+}
