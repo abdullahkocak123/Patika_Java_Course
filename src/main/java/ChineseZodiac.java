3bbd809f4df28ad820b37de252035e738678f2df
Abdullah <abdullahkocak33@hotmail.com>
2025-03-06 09:50:00 +0300

    ChineseZodiac example added

---
+++ src/main/java/ChineseZodiac.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ChineseZodiac {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz: ");
+
+        int age = scanner.nextInt();
+
+        String zodiac = switch (age % 12) {
+            case 0 -> "Maymun";
+            case 1 -> "Horoz";
+            case 2 -> "Köpek";
+            case 3 -> "Domuz";
+            case 4 -> "Fare";
+            case 5 -> "Öküz";
+            case 6 -> "Kaplan";
+            case 7 -> "Tavşan";
+            case 8 -> "Ejderha";
+            case 9 -> "Yılan";
+            case 10 -> "At";
+            case 11 -> "Koyun";
+            default -> "Geçersiz gün!";
+        };
+
+        System.out.println("Çin Zodyağı Burcunuz: " + zodiac);
+    }
+}
