fa620c12bd093e7ef4db4c73d9c2031fdfe9259f
Abdullah <abdullahkocak33@hotmail.com>
2025-03-10 10:33:21 +0300

    Casting example added

---
+++ src/main/java/Casting.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Casting {
+    public static void main(String[] args) {
+
+        //Kullanıcıdan tip dönüşümü yapmak üzere bir tam sayı ve bir ondalıklı sayı alalım;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Lütfen bir tam sayı giriniz: ");
+        int tamsayi = scanner.nextInt();
+        System.out.print("Lütfen bir ondalıklı sayı giriniz: ");
+        double ondalikli = scanner.nextDouble();
+
+        //Tam sayıyı ondalıklı sayıya çevirelim;
+        double ondalikli2 = (double) tamsayi;
+
+        //Ondalıklı sayıyı tam sayıya çevirelim;
+        int tamsayi2 = (int) ondalikli;
+
+        //Sonuçları ekrana yazdıralım;
+        System.out.print("Tam sayının ondalıklı hali: " + ondalikli2);
+        System.out.println();
+        System.out.print("Ondalıklı sayının tam sayı hali: " + tamsayi2);
+    }
+}
