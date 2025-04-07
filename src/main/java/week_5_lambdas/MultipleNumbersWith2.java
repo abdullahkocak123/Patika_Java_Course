9af3e162d40ea95ebb487a453f20a3d62d1a77b5
Abdullah <abdullahkocak33@hotmail.com>
2025-04-07 18:12:47 +0300

    Lambda example added

---
+++ src/main/java/week_5_lambdas/MultipleNumbersWith2.java
@@ -0,0 +1,19 @@
+package week_5_lambdas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultipleNumbersWith2 {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        //Using lambda instead of standard for loops; both multiplying by 2 and printing numbers
+        numbers.forEach(number -> System.out.println(number * 2));
+
+    }
+}
